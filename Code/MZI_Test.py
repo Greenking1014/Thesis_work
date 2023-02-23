9024cf09d1ae3c9ac0213db65c324a9b88550441
Carlos Gonzalez <77692383+Greenking1014@users.noreply.github.com>
2023-02-23 16:39:05 -0700

    Just finished creating the first MZI_X_3 need to make the 4 other ones.

--- Code/MZI_Test.py
+++ Code/MZI_Test.py
@@ -1,13 +1,36 @@
-import math
 from MZI import MZI 
-x = complex(0,1)
+import sympy as sym
+from sympy import symbols
 
 
-mzi1 = MZI(0)
-mzi2 = MZI(1)
+####### Testing Area #######
+# Bar config
+# mzi1 = MZI(0)
 
-print(mzi1.name)
-print(mzi1.matrix_eq)
+# # Cross config
+# mzi2 = MZI(1)
 
-print(mzi2.name)
-print(mzi2.matrix_eq)
+# print(mzi1.name)
+# print(mzi1.matrix_eq)
+# print(mzi1.matrix_eq[0,0])
+# print(mzi2.name)
+# print(mzi2.matrix_eq)
+
+####### Testing Area #######
+
+# FOR INPUT X_3_X_2_X_1_X_0 = 0100
+#First MZI X_3
+
+G_o = symbols('G_o')
+mziX_3 = MZI(0)
+print(mziX_3.matrix_eq)
+Input_1 = sym.Matrix([[1],[G_o]])
+F_o_F_1 =([mziX_3.matrix_eq*Input_1])
+print(F_o_F_1)
+
+#Second MZI X_2
+
+G_3 = F_o_F_1[0][1]
+
+
+
