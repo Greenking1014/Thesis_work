650a5cf98785d642f919a74fba38c467db6f0f7f
Carlos Gonzalez <77692383+Greenking1014@users.noreply.github.com>
2023-04-07 20:18:16 -0600

    Fixed the MZI class. Need to run all 16 test script.

--- Code/MZI.py
+++ Code/MZI.py
@@ -2,34 +2,28 @@ from sympy import *
 
 import cmath
 class MZI:
-    def __init__(self, config_selection, p_Input, q_Input): #Could pass in a deformity variable def
+    def __init__(self, config_selection, input): #Could pass in a deformity variable def
         # self.phi_1 = np.pi
         # self.phi_2 = 0
         self.x = complex(0,1)
         self.r = 1
-        self.p_Input = p_Input
-        self.q_Input = q_Input
-        self.f_Output = None
-        self.g_Output = None
+        self.input = input
+        self.output = None
         if (config_selection == 0):
             matrix1 = Matrix ([[0.707, -0.707*self.x],[-0.707*self.x, 0.707]])
             matrix2 = matrix1.multiply(Matrix( [[-1,0],[0,1]]))
             matrix3 = Matrix([[0.707, -0.707*self.x],[-0.707*self.x, 0.707]])
             self.matrix_eq = matrix2.multiply(matrix3)
-            if(p_Input != None):
-                self.f_Output = [self.matrix_eq * self.p_Input]     
-            if(q_Input != None):
-                self.g_Output = [self.matrix_eq * self.q_Input]
+            if(input != None):
+                self.output = [self.matrix_eq * self.input]
             self.name = "Matrix MZI bar_configuration (S=0)"
         elif (config_selection == 1):
             matrix1 = Matrix ([[0.707, -0.707*self.x],[-0.707*self.x, 0.707]])
             matrix2 = matrix1.multiply(Matrix( [[1,0],[0,1]]))
             matrix3 = Matrix([[0.707, -0.707*self.x],[-0.707*self.x, 0.707]])
             self.matrix_eq = matrix2.multiply(matrix3)
-            if(p_Input != None):
-                self.g_Output = [self.matrix_eq * self.p_Input]
-            if(q_Input != None):
-                self.f_Output = [self.matrix_eq * self.q_Input]
+            if(input != None):
+                self.output = [self.matrix_eq * self.input]
             self.name = "Matrix MZI cross_configuration (S=1)"
         elif (config_selection == 2):
             self.phi_1 = 2.4
@@ -37,10 +31,8 @@ class MZI:
             matrix2 = Matrix([[cmath.rect(self.r,self.phi_1),0],[0,cmath.rect(self.r,0)]])
             matrix3 = Matrix([[0.707, -0.707*self.x],[-0.707*self.x, 0.707]])
             self.matrix_eq = matrix3.multiply(matrix2.multiply(matrix1))
-            if(p_Input != None):
-                self.f_Output = [self.matrix_eq * self.p_Input]    
-            if(q_Input != None):
-                self.g_Output = [self.matrix_eq * self.q_Input]
+            if(input != None):
+                self.output = [self.matrix_eq * self.input]
             self.name = "Matrix MZI bar_configuration (S=0) deformed"
         
         
--- Code/Updated_MZI_Test.py
+++ Code/Updated_MZI_Test.py
@@ -22,25 +22,111 @@ import numpy as np
 
 # FOR INPUT X_3_X_2_X_1_X_0 = 0100
 #First MZI X_3
-print('First MZI X_3')
+print('First MZI S = X_3')
 G_o = symbols('G_o')
-Input = sym.Matrix([[1],[G_o]])
-mziX_3 = MZI(0,Input,None)
-print(mziX_3.name)
-print(mziX_3.matrix_eq)
+Input = sym.Matrix([[1],[G_o]]) # [1] goes to P and [G_o] goes to q
+mzi_1 = MZI(0,Input)
+print(mzi_1.name)
+print(mzi_1.matrix_eq)
 
-F_o_F_1 =(mziX_3.f_Output)
+F_o_F_1 =(mzi_1.output)
 print(F_o_F_1)
 
-print('')
+print('---------------------------------------')
 
 #Second MZI X_2
-print('Second MZI X_2')
+print('Second MZI S = X_2')
 G_3 = F_o_F_1[0][1]
 G_1 = symbols('G_1')
 Input_2 = sym.Matrix([[G_1],[G_3]])
-mziX_2 = MZI(1,None,Input_2)
-print(mziX_2.name)
-print(mziX_2.matrix_eq)
-F_2_F_3 = (mziX_2.g_Output)
+mzi_2 = MZI(1,Input_2)
+print(mzi_2.name)
+print(mzi_2.matrix_eq)
+F_2_F_3 = (mzi_2.output)
 print(F_2_F_3)
+
+print('---------------------------------------')
+
+#Third MZI X_1
+print('Third MZI S = X_1')
+G_4 = F_2_F_3[0][1]
+G_5 = F_o_F_1[0][0]
+Input_3 = sym.Matrix([[G_4],[G_5]])
+print(Input_3)
+mzi_3 = MZI(0,Input_3)
+print(mzi_3.name)
+print(mzi_3.matrix_eq)
+Z_i_F_4 = (mzi_3.output)
+print(Z_i_F_4)
+
+print('---------------------------------------')
+
+#Fourth MZI X_3
+print('Fourth MZI S = X_3')
+G_6 = symbols('G_6')
+G_7 = Z_i_F_4[0][1]
+Input_4 = sym.Matrix([[G_6],[G_7]])
+mzi_4 = MZI(0,Input_4)
+print(mzi_4.name)
+print(mzi_4.matrix_eq)
+F_5_F_6 = (mzi_4.output)
+print(F_5_F_6)
+
+print('---------------------------------------')
+
+# #Fifth MZI S = X_0
+# print('Fifth MZI S = X_0')
+# G_8 = F_5_F_6[0][0]
+# Input_5 = sym.Matrix([[0], [G_8]])
+# mzi_5= MZI(0,Input_5)
+# print(mzi_5.name)
+# print(mzi_5.matrix_eq)
+# F_7_F_8 = (mzi_5.output)
+# print (F_7_F_8)
+
+# print('---------------------------------------')
+
+#Deformed Fifth MZI X_0
+print('Deformed Fifth MZI S = X_0')
+G_8 = F_5_F_6[0][1]
+Input_5 = sym.Matrix([[0], [G_8]])
+mzi_5_Defor = MZI(2,Input_5)
+print(mzi_5_Defor.name)
+print(mzi_5_Defor.matrix_eq)
+F_7_F_8 = (mzi_5_Defor.output)
+print(F_7_F_8)
+
+print('---------------------------------------')
+
+#Using First MZI X_3
+print('First MZI S = X_3')
+G_6 = F_7_F_8[0][0]
+G_7 = Z_i_F_4[0][1]
+Input_4 = sym.Matrix([[G_6],[G_7]])
+F_5_F_6 = ([mzi_1.matrix_eq*Input_4])  # Uses the first mzi matrix equation
+                                #Should I keep it this way to make sure no new Mzi.
+                                # also how does this translate to the file reader? 
+                                # there is an order it goes in so how can i make sure it follows that order?
+                                
+print(F_5_F_6)
+
+print('---------------------------------------')
+
+# Using Second MZI X_2
+print('Second MZI S = X_2')
+G_1 = F_5_F_6[0][0]
+G_3 = F_o_F_1[0][1]
+Input_2 = sym.Matrix([[G_1],[G_3]])
+F_2_F_3 = ([mzi_2.matrix_eq*Input_2])
+print(F_2_F_3)
+
+print('---------------------------------------')
+
+# Using Third MZI X_1
+print('Third MZI S = X_1')
+G_4 = F_2_F_3[0][1]
+G_5 = F_o_F_1[0][0]
+Input_3 = sym.Matrix([[G_4],[G_5]])
+Z_i_F_4 = ([mzi_3.matrix_eq*Input_3])
+print(Z_i_F_4)
+print(np.abs(Z_i_F_4))
